dee916067f43ac6e16b9ad95f476023aa1abe52a
NayoungKim1212 <kkd04250@naver.com>
2023-06-30 14:00:10 +0900

    로그인

--- src/main/java/com/sparta/post/controller/UserController.java
+++ src/main/java/com/sparta/post/controller/UserController.java
@@ -1,23 +1,23 @@
-//package com.sparta.post.controller;
-//
-//import com.sparta.post.dto.SignupRequestDto;
-//import com.sparta.post.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequiredArgsConstructor
-//@RequestMapping("/api")
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//    @PostMapping("/user/signup")
-//    public String signup(SignupRequestDto requestDto) {
-//        userService.signup(requestDto);
-//    }
-//
-//}
+package com.sparta.post.controller;
+
+import com.sparta.post.dto.SignupRequestDto;
+import com.sparta.post.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+    }
+
+}
