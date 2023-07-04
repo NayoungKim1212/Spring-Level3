9688588a2cd7357b2cef7750415778c0093bb5ad
NayoungKim1212 <kkd04250@naver.com>
2023-07-04 20:12:44 +0900

    Fix : 정규식 수정, 불필요한 username 삭제, RestContoller로 수정

--- src/main/java/com/sparta/post/dto/UserRequestDto.java
+++ src/main/java/com/sparta/post/dto/UserRequestDto.java
@@ -8,10 +8,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserRequestDto {
-    @Pattern(regexp = "^[a-z_0-9]{4,10}$", message = "소문자와 숫자를 포함한 4~10자 이내로 작성해주세요")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "소문자와 숫자를 포함한 4~10자 이내로 작성해주세요")
     @NotBlank
     private String username;
-    @Pattern(regexp = "^[a-z_0-9_A-Z]{8,15}$", message = "대소문자와 숫자를 포함한 8~15자 이내로 작성해주세요")
+    @Pattern(regexp = "^[a-z_0-9A-Z]{8,15}$", message = "대소문자와 숫자를 포함한 8~15자 이내로 작성해주세요")
     @NotBlank
     private String password;
 
