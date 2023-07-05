5a59e6f8fe17e1146d5eb4835c75609a37c482dc
Kkangjn <jjun1401@naver.com>
2023-07-05 23:00:19 +0900

    fix: Spring Security를 사용하지 않게 수정

--- src/main/java/com/sparta/post/service/PostService.java
+++ src/main/java/com/sparta/post/service/PostService.java
@@ -3,11 +3,13 @@ package com.sparta.post.service;
 import com.sparta.post.dto.PostRequestDto;
 import com.sparta.post.dto.PostResponseDto;
 import com.sparta.post.entity.Post;
+import com.sparta.post.jwt.JwtUtil;
 import com.sparta.post.repository.PostRepository;
+import io.jsonwebtoken.Claims;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +19,14 @@ import java.util.List;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final JwtUtil jwtUtil;
 
-    public PostResponseDto createPost(PostRequestDto requestDto) {
-        String username = getUsername();
+    public ResponseEntity<PostResponseDto> createPost(String tokenValue, PostRequestDto requestDto) {
+        String token = authentication(tokenValue);
+        String username = getUsernameFromJwt(token);
         Post post = new Post(requestDto, username);
         postRepository.save(post);
-        return new PostResponseDto(post);
+        return new ResponseEntity<>(new PostResponseDto(post), HttpStatus.OK);
     }
 
     public List<PostResponseDto> getPosts() {
@@ -38,25 +42,22 @@ public class PostService {
     }
 
     @Transactional
-    public PostResponseDto updatePost(Long id, PostRequestDto requestDto) {
+    public ResponseEntity<PostResponseDto> updatePost(String tokenValue, Long id, PostRequestDto requestDto) {
         Post post = findPost(id);
-        String username = getUsername();
-        if (username.equals(post.getUsername())) {
-            post.update(requestDto);
-            return new PostResponseDto(post);
-        } else {
-            throw new IllegalArgumentException("잘못된 사용자입니다.");
-        }
+        String token = authentication(tokenValue);
+        String username = getUsernameFromJwt(token);
+        usernameMatch(username, post.getUsername());
+        post.update(requestDto);
+        return new ResponseEntity<>(new PostResponseDto(post), HttpStatus.OK);
     }
 
-    public void deletePost(Long id) {
+    public ResponseEntity<?> deletePost(String tokenValue, Long id) {
         Post post = findPost(id);
-        String username = getUsername();
-        if (username.equals(post.getUsername())) {
-            postRepository.delete(post);
-        } else {
-            throw new IllegalArgumentException("잘못된 사용자입니다.");
-        }
+        String token = authentication(tokenValue);
+        String username = getUsernameFromJwt(token);
+        usernameMatch(username, post.getUsername());
+        postRepository.delete(post);
+        return ResponseEntity.status(HttpStatus.OK).body("게시글이 삭제 되었습니다.");
     }
 
     private Post findPost(Long id) {
@@ -65,41 +66,25 @@ public class PostService {
         );
     }
 
-    public String getUsername() {
-        System.out.println("이름 받아오는중");
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // SecurityContextHolder내부에 SecurityContext내부에 있는 Authentication을 받아옴
-        System.out.println("authentication.getName() = " + authentication.getName());
-        if (authentication != null) {
-            return authentication.getName(); // Authentication에서 이름을 받아온다.
+    private void usernameMatch(String loginUsername, String postUsername) {
+        if (!loginUsername.equals(postUsername)){
+            throw new IllegalArgumentException("잘못된 사용자입니다.");
         }
-        return null;
+    }
+
+    private String authentication(String tokenValue) { // 유효한 토큰인지 확인하고 토큰 반환
+        String decodedToken = jwtUtil.decodingToken(tokenValue);
+        String token = jwtUtil.substringToken(decodedToken);
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("Token Error");
+        }
+        return token;
+    }
+    private String getUsernameFromJwt(String token) { // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        String username = info.getSubject();
+        return username;
     }
 }
-
-//    private boolean isTokenValid(String token, Post post) {
-//
-//        String username = getUsernameFromJwt(token);
-//
-//        if (username.equals(post.getUsername())) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//
-//    private String getUsernameFromJwt(String tokenValue) {
-//
-//        String token = jwtUtil.substringToken(tokenValue);
-//
-//        if (!jwtUtil.validateToken(tokenValue)) {
-//
-//            throw new IllegalArgumentException("Token Error");
-//        }
-//
-//        // 토큰에서 사용자 정보 가져오기
-//        Claims info = jwtUtil.getUserInfoFromToken(token);
-//        String username = info.getSubject();
-//        return username;
-//    }
 
 
